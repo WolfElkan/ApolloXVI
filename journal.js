96faeb697304e5c715d6bc0e880578c4ba1b5b2e
Wolf Elkan <wolfelkan@github.com>
2022-04-16 13:26:22 -0400

    Updated for Apollo XVI

---
+++ journal.js
@@ -0,0 +1,28 @@
+var afj = [
+	{'GET':-Infinity,   'page':'https://history.nasa.gov/afj/ap15fj/01launch_to_earth_orbit.html'},
+	{'GET':"000:16:56", 'page':'https://history.nasa.gov/afj/ap15fj/02earth_orbit_tli.html'},
+	{'GET':"003:01:00", 'page':'https://history.nasa.gov/afj/ap15fj/03tde.html'},
+	{'GET':"005:25:53", 'page':'https://history.nasa.gov/afj/ap15fj/04troubleshoot_ptc.html'},
+	{'GET':"015:00:00", 'page':'https://history.nasa.gov/afj/ap15fj/05day2_checking_sps.html'},
+	{'GET':"031:31:31", 'page':'https://history.nasa.gov/afj/ap15fj/06day2_enter_lm.html'},
+	{'GET':"040:00:00", 'page':'https://history.nasa.gov/afj/ap15fj/07day3_flashing_lights.html'},
+	{'GET':"055:10:00", 'page':'https://history.nasa.gov/afj/ap15fj/08day3_leak_hilltop.html'},
+	{'GET':"065:00:00", 'page':'https://history.nasa.gov/afj/ap15fj/09day4_lunar_encounter.html'},
+	{'GET':"079:00:00", 'page':'https://history.nasa.gov/afj/ap15fj/10a-day4_lunar_orbit1+2.html'},
+	{'GET':"082:28:06", 'page':'https://history.nasa.gov/afj/ap15fj/10b-day4_doi-rest.html'},
+	{'GET':"087:00:00", 'page':'https://history.nasa.gov/afj/ap15fj/11day5_wakeup.html'},
+	{'GET':"095:38:10", 'page':'https://history.nasa.gov/afj/ap15fj/12a-day5_doi_trim.html'},
+	{'GET':"096:53:00", 'page':'https://history.nasa.gov/afj/ap15fj/12b-day5_lm_activation.html'},
+	{'GET':"099:25:00", 'page':'https://history.nasa.gov/afj/ap15fj/12c-day5_lm_undock.html'},
+	{'GET':"101:03:30", 'page':'https://history.nasa.gov/afj/ap15fj/12d-day5_csm_circ.html'},
+	{'GET':"102:00:00", 'page':'https://history.nasa.gov/afj/ap15fj/12e-day5_landing_prep.html'},
+	{'GET':"105:00:00", 'page':'https://history.nasa.gov/afj/ap15fj/13solo_ops1.html'},
+	{'GET':"", 'page':''},
+	{'GET':"", 'page':''},
+	{'GET':"", 'page':''},
+	{'GET':"", 'page':''},
+]
+
+var alsj = [
+
+]
